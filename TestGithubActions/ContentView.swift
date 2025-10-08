15eb05032f8b291ec7902d7d6d3a153073aefe7c
Ramdan <dadanisme@upi.edu>
2025-10-08 12:45:34 +0800

    Initial Commit

---
+++ TestGithubActions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestGithubActions
+//
+//  Created by Ramdan on 08/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestGithubActions/TestGithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubActionsApp.swift
+//  TestGithubActions
+//
+//  Created by Ramdan on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
